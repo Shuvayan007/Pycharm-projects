ac0f22528aa0d267271eb9d6bdb6f72463002123
Shuvayan007 <shuvayanpal2000@gmail.com>
2020-11-18 22:03:43 +0530

    From Laptop:18-11-2020

--- LinearRegression/venv/Scripts/activate.bat
+++ LinearRegression/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\shuva\Github_repositories\Pycharm_projects\LinearRegression\venv
+set VIRTUAL_ENV=C:\Users\shuva\Git_repositories\Pycharm_projects\LinearRegression\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
