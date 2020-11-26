17a4e3908aa4dc4848e7e2590f4342d43bcbb451
Shuvayan007 <shuvayanpal2000@gmail.com>
2020-11-26 13:27:56 +0530

    From Laptop:26-11-2020

---
+++ LinearRegression/SGDRegressor.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+
+class SGD:
+    def __init__(self, learning_rate=0.001, epochs=10000):
+        self.coef_ = None
+        self.intercept_ = None
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+
+    def fit(self, X_train, y_train):
+        # Start with random values of beta:
+        self.intercept_ = 0
+        self.coef_ = np.ones(X_train.shape[1])
+        print('_________________________________________________________________')
+
+        # update
+        for epoch in range(self.epochs):
+
+            for i in range (X_train.shape[0]):
+
+                y_hat = np.dot(X_train[i], self.coef_) + self.intercept_
+
+                i_der = -2 * (y_train[i] - y_hat)
+                self.intercept_ = self.intercept_ - self.learning_rate * i_der
+
+                c_der = -2 * (np.dot(y_train[i] - y_hat, X_train[i]) / X_train.shape[0])
+
+                self.coef_ = self.coef_ - self.learning_rate * c_der
+
+            # print(self.intercept_)
+            # print(self.coef_)
+
+            # Calculate the loss
+            # loss=np.dot(y_train-y_hat,y_train-y_hat)/X_train.shape[0]
+            #
+            # print(loss)
+
+    def predict(self, X_test):
+        y_pred = np.dot(X_test, self.coef_) + self.intercept_
+        return y_pred
--- LinearRegression/app.py
+++ LinearRegression/app.py
@@ -2,27 +2,61 @@ from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from LinearRegression import MyLR
+from gradientDescent import LinearRegressionGD
+from SGDRegressor import SGD
 
 X,y=datasets.load_diabetes(return_X_y=True)
 
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=1)
 
+#Batch Gradient Descent-->
+
+reg=LinearRegressionGD(epochs=10000,learning_rate=0.1)
+
+reg.fit(X_train,y_train)
+
+y_pred= reg.predict(X_test)
+
+from sklearn.metrics import r2_score
+print('Batch GD',r2_score(y_test,y_pred))
+
+#Stokastic Gradient Descent-->
+
+sgd=SGD(epochs=10000,learning_rate=0.1)
+
+sgd.fit(X_train,y_train)
+
+y_pred1=sgd.predict(X_test)
+
+print('SGD',r2_score(y_test,y_pred1))
+
+#Mini-batch Gradient Descent-->
+
+
+
+#OLE:
+
 # print(X_train.shape)
 # print(y_train.shape)
 
 # reg=LinearRegression()
-reg1=MyLR()
 
-reg1.fit(X_train,y_train)
-
-y_pred=reg1.predict(X_test)
+# reg1=MyLR()
+#
+# reg1.fit(X_train,y_train)
+#
+# y_pred=reg1.predict(X_test) #Needed
 
 # reg.fit(X_train,y_train)
 
 # y_pred=reg.predict((X_test))
 
-from sklearn.metrics import r2_score
-print(r2_score(y_test,y_pred))
+# from sklearn.metrics import r2_score
+# print(r2_score(y_test,y_pred)) #Needed
 
 # print(reg.coef_)
 # print(reg.intercept_)
+
+
+#Assignment:
+#Mini batch implementation to be done
